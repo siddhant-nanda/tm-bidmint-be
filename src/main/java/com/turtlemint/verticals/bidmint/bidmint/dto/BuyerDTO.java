d88d00a48fded83608dadcffa9d0b3e4dd01c2d2
Siddhant Nanda <siddhantnanda03@gmail.com>
2021-06-24 13:51:46 +0530

    Add create controller

---
+++ src/main/java/com/turtlemint/verticals/bidmint/bidmint/dto/BuyerDTO.java
@@ -0,0 +1,8 @@
+package com.turtlemint.verticals.bidmint.bidmint.dto;
+
+import com.turtlemint.verticals.bidmint.bidmint.dao.Buyer;
+
+public class BuyerDTO extends BidMintDTO {
+
+    Buyer buyer;
+}
